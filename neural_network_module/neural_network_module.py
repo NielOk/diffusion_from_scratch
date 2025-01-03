2522a0d610c86658027c09f19e411ea6779b6fbc
Niel Ok <nielok@stanford.edu>
2025-01-02 19:24:34 -0500

    added batching for data

--- neural_network_module/neural_network_module.py
+++ neural_network_module/neural_network_module.py
@@ -11,6 +11,25 @@ class NeuralNetwork:
         self.layers = {} # dictionary to store information about the layers of the neural network, including activations, number of neurons, number of inputs, etc.
         self.layer_weights = {} # dictionary to store the tensor weights for each layer. weights are stacked as rows for each layer.
         self.layer_biases = {} # dictionary to store the vector biases for each layer
+
+
+    ### DATA PREPARATION METHODS ###
+    def create_batches(self, 
+                       data: np.ndarray, # data to be batched, is a singular numpy array where each row is a data item
+                       batch_size: int # size of each batch
+                       ) -> np.ndarray:
+        '''
+        Create batches from a data array. This is to be used for both features and labels (the batch size should be the same for both for separate datasets).
+        '''
+        num_samples = data.shape[0]
+        # Compute the number of batches
+        num_batches = int(np.ceil(num_samples / batch_size))
+        
+        # Use slicing to create batches
+        batches = [data[i * batch_size:(i + 1) * batch_size] for i in range(num_batches)]
+        return batches
+            
+    ### NEURAL NETWORK METHODS ###
         
     def add_dense_layer(self,
                         num_inputs: int, # length of input vector to the layer. inputs must be flattened before being passed to the layer in forward propagation.
--- neural_network_module/test_neural_net_module.py
+++ neural_network_module/test_neural_net_module.py
@@ -17,5 +17,21 @@ def test1(): # Check dense layer definition and initialization
         print(net.layer_biases[i].shape)
         print(net.layer_biases[i])
 
+def test2(): # Check batching
+    net = NeuralNetwork()
+    data = np.random.randn(100, 32*32*3)
+    labels = np.random.randn(100, 1)
+    batch_size = 10
+    data_batches = net.create_batches(data, 10)
+    label_batches = net.create_batches(labels, 10)
+
+    print(len(data_batches))
+    print(len(label_batches))
+
+    for i in range(len(data_batches)):
+        print(data_batches[i].shape)
+        print(label_batches[i].shape)
+    
 if __name__ == '__main__':
-    test1()
+    #test1()
+    test2()
